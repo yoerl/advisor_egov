7faa7a102ac2bcaa92109b36943b0a14e3c1a92b
“yoerl” <jokerbook365@gmail.com>
2023-09-01 17:10:10 +0900

    init

--- advisor_message_egov/src/main/java/egov/sample/stomp/controller/StompController.java
+++ advisor_message_egov/src/main/java/egov/sample/stomp/controller/StompController.java
@@ -44,85 +44,159 @@ public class StompController {
 	private SimpMessageSendingOperations messagingTemplate;
 
 	
+//
+//	@MessageMapping("/recvRabbit")
+//	public void sendToAll2() throws Exception {
+//	    ConnectionFactory factory = new ConnectionFactory();
+//
+//	    factory.setHost(rabbitmqProperties.getProperty("Globals.rabbitmq.Host"));
+//	    factory.setPort(Integer.parseInt(rabbitmqProperties.getProperty("Globals.rabbitmq.Port").toString()));
+//	    factory.setUsername(rabbitmqProperties.getProperty("Globals.rabbitmq.Username"));
+//	    factory.setPassword(rabbitmqProperties.getProperty("Globals.rabbitmq.Password"));
+//	    
+//	    
+//        executorService.execute(() -> {
+//            try {
+//                while (!connectionClosed) {
+//                    Connection connection = factory.newConnection();
+//                    Channel channel = connection.createChannel();
+//
+//    	            channel.queueDeclare("hello", false, false, false, null);
+//    	            System.out.println(" [*] Waiting for messages. To exit press CTRL+C_a");
+//    
+//    	            Consumer consumer = new DefaultConsumer(channel) {
+//    	                @Override
+//    	                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+//    	                    String message = new String(body, "UTF-8");
+//    	                    System.out.println("received : " + message);
+//    
+//    	                    messagingTemplate.convertAndSend("/topic/message", message);
+//    	                }
+//    	            };
+//    	            channel.basicConsume("hello", true, consumer);
+//
+//                    connection.addShutdownListener(cause -> connectionClosed = true);
+//                }
+//            } catch (Exception e) {
+//                connectionClosed = true;
+//            }
+//        });
+//        
+//	}
+//
+//	@MessageMapping("/recvRabbit_a")
+//	public void sendToAll2_a() throws Exception {
+//	    ConnectionFactory factory = new ConnectionFactory();
+//
+//	    factory.setHost(rabbitmqProperties.getProperty("Globals.rabbitmq.Host"));
+//	    factory.setPort(Integer.parseInt(rabbitmqProperties.getProperty("Globals.rabbitmq.Port").toString()));
+//	    factory.setUsername(rabbitmqProperties.getProperty("Globals.rabbitmq.Username"));
+//	    factory.setPassword(rabbitmqProperties.getProperty("Globals.rabbitmq.Password"));
+//	    
+//	    
+//        executorService.execute(() -> {
+//            try {
+//                while (!connectionClosed) {
+//                    Connection connection = factory.newConnection();
+//                    Channel channel = connection.createChannel();
+//
+//    	            channel.queueDeclare("hello_a", false, false, false, null);
+//    	            System.out.println(" [*] Waiting for messages. To exit press CTRL+C_a");
+//    
+//    	            Consumer consumer = new DefaultConsumer(channel) {
+//    	                @Override
+//    	                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+//    	                    String message = new String(body, "UTF-8");
+//    	                    System.out.println("received_a : " + message);
+//    
+//    	                    messagingTemplate.convertAndSend("/topic/message_a", message);
+//    	                }
+//    	            };
+//    	            channel.basicConsume("hello_a", true, consumer);
+//
+//                    connection.addShutdownListener(cause -> connectionClosed = true);
+//                }
+//            } catch (Exception e) {
+//                connectionClosed = true;
+//            }
+//        });
+//	}
+
 
 	@MessageMapping("/recvRabbit")
 	public void sendToAll2() throws Exception {
-	    ConnectionFactory factory = new ConnectionFactory();
-
-	    factory.setHost(rabbitmqProperties.getProperty("Globals.rabbitmq.Host"));
-	    factory.setPort(Integer.parseInt(rabbitmqProperties.getProperty("Globals.rabbitmq.Port").toString()));
-	    factory.setUsername(rabbitmqProperties.getProperty("Globals.rabbitmq.Username"));
-	    factory.setPassword(rabbitmqProperties.getProperty("Globals.rabbitmq.Password"));
-	    
-	    
-        executorService.execute(() -> {
-            try {
-                while (!connectionClosed) {
-                    Connection connection = factory.newConnection();
-                    Channel channel = connection.createChannel();
-
-    	            channel.queueDeclare("hello", false, false, false, null);
-    	            System.out.println(" [*] Waiting for messages. To exit press CTRL+C_a");
-    
-    	            Consumer consumer = new DefaultConsumer(channel) {
-    	                @Override
-    	                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-    	                    String message = new String(body, "UTF-8");
-    	                    System.out.println("received : " + message);
-    
-    	                    messagingTemplate.convertAndSend("/topic/message", message);
-    	                }
-    	            };
-    	            channel.basicConsume("hello", true, consumer);
-
-                    connection.addShutdownListener(cause -> connectionClosed = true);
-                }
-            } catch (Exception e) {
-                connectionClosed = true;
-            }
-        });
+		ConnectionFactory factory = new ConnectionFactory();
         
-	}
+        factory.setHost(rabbitmqProperties.getProperty("Globals.rabbitmq.Host"));
+        factory.setPort(Integer.parseInt(rabbitmqProperties.getProperty("Globals.rabbitmq.Port").toString()));
+        factory.setUsername(rabbitmqProperties.getProperty("Globals.rabbitmq.Username"));
+        factory.setPassword(rabbitmqProperties.getProperty("Globals.rabbitmq.Password"));
+        
 
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        // Exchange 선언 (선택사항)
+        // channel.exchangeDeclare(exchangeName, BuiltinExchangeType.DIRECT, true);
+
+        // 지정된 이름의 큐를 선언합니다. 이 큐에서 메시지를 가져옵니다.
+        channel.queueDeclare("hello", false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // 메시지를 저장할 변수
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("received : " + message);
+                
+                messagingTemplate.convertAndSend("/topic/message", message);
+                
+            }
+        };
+        channel.basicConsume("hello", true, consumer);
+        
+		
+	}
 	@MessageMapping("/recvRabbit_a")
 	public void sendToAll2_a() throws Exception {
-	    ConnectionFactory factory = new ConnectionFactory();
+		ConnectionFactory factory = new ConnectionFactory();
+        
+        factory.setHost(rabbitmqProperties.getProperty("Globals.rabbitmq.Host"));
+        factory.setPort(Integer.parseInt(rabbitmqProperties.getProperty("Globals.rabbitmq.Port").toString()));
+        factory.setUsername(rabbitmqProperties.getProperty("Globals.rabbitmq.Username"));
+        factory.setPassword(rabbitmqProperties.getProperty("Globals.rabbitmq.Password"));
+        
 
-	    factory.setHost(rabbitmqProperties.getProperty("Globals.rabbitmq.Host"));
-	    factory.setPort(Integer.parseInt(rabbitmqProperties.getProperty("Globals.rabbitmq.Port").toString()));
-	    factory.setUsername(rabbitmqProperties.getProperty("Globals.rabbitmq.Username"));
-	    factory.setPassword(rabbitmqProperties.getProperty("Globals.rabbitmq.Password"));
-	    
-	    
-        executorService.execute(() -> {
-            try {
-                while (!connectionClosed) {
-                    Connection connection = factory.newConnection();
-                    Channel channel = connection.createChannel();
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
 
-    	            channel.queueDeclare("hello_a", false, false, false, null);
-    	            System.out.println(" [*] Waiting for messages. To exit press CTRL+C_a");
-    
-    	            Consumer consumer = new DefaultConsumer(channel) {
-    	                @Override
-    	                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-    	                    String message = new String(body, "UTF-8");
-    	                    System.out.println("received_a : " + message);
-    
-    	                    messagingTemplate.convertAndSend("/topic/message_a", message);
-    	                }
-    	            };
-    	            channel.basicConsume("hello_a", true, consumer);
+        // Exchange 선언 (선택사항)
+        // channel.exchangeDeclare(exchangeName, BuiltinExchangeType.DIRECT, true);
 
-                    connection.addShutdownListener(cause -> connectionClosed = true);
-                }
-            } catch (Exception e) {
-                connectionClosed = true;
+        // 지정된 이름의 큐를 선언합니다. 이 큐에서 메시지를 가져옵니다.
+        channel.queueDeclare("hello_a", false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C_a");
+
+        // 메시지를 저장할 변수
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println("received_a : " + message);
+                
+                messagingTemplate.convertAndSend("/topic/message_a", message);
+                
             }
-        });
+        };
+        channel.basicConsume("hello_a", true, consumer);
+        
+		
 	}
-
-
+	
+	
 	@MessageMapping("/sendRabbit")
 	public void sendToRabbit(UserMessage userMessage) throws Exception {
 		log.debug("===>>> sendToAll >>" + userMessage);
